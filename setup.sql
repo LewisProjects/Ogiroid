1bbc83559add762312a88f5585a4d269987e8d2a
FreebieII <fredzapke9@gmail.com>
2022-06-23 23:23:50 +0200

    Formatted

--- setup.sql
+++ setup.sql
@@ -1,9 +1,13 @@
-CREATE TABLE IF NOT EXISTS tags (
+CREATE TABLE
+IF NOT EXISTS tags
+(
     tag_id TEXT,
     content TEXT
 );
 
-CREATE TABLE IF NOT EXISTS tickets (
+CREATE TABLE
+IF NOT EXISTS tickets
+(
     channel_id TEXT,
     user_id TEXT
 );
