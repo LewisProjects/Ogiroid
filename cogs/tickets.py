5a8a543167d99d5a0bf298bb83e07e3f80ebe74a
FreebieII <fredzapke9@gmail.com>
2022-06-26 21:59:08 +0200

    *Just gives up*

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -15,9 +15,7 @@ class Tickets(commands.Cog):
             await message.add_reaction(emoji)
 
 
-
-
-
+"""
         @commands.Cog.listener()
         async def on_raw_reaction_add(self, payload):
             reaction_message = await self.client.get_channel(990679557596135475).fetch_message(990679907795349554)
@@ -25,7 +23,11 @@ class Tickets(commands.Cog):
                 user = self.client.get_user(payload.user_id)
                 if user.id == 984802008403959879:
                     return print('Added reaction from bot')
-                    
+                guild = self.client.get_guild(payload.guild_id)
+                role = await guild.create_role(
+                    name=f"{user.id}",hoist=False,colour=int("FFFFFF", 16), mentionable=False, reason="Tickets"
+                    )
+                await self.client.add_roles(user, role)
                 staff = self.client.get_guild(985234686878023730).get_role(985943266115584010)
                 emoji = self.client.get_emoji(990310706874290216)
                 ticket_channel = await reaction_message.guild.create_text_channel(f"ticket-{user.name}")
@@ -35,20 +37,18 @@ class Tickets(commands.Cog):
                 message_content = "Thank you for contacting support! A staff member will be here shortly!"
                 em = disnake.Embed(title=f"Ticket made by {user.name}#{user.discriminator}", description= f"{message_content}", color=0x26ff00)
                 em.set_footer(text="ticket", icon_url=user.avatar.url)
-                await ticket_channel.send(embed=em)
+                await ticket_channel.send(embed=em + f"Thank you for contacting support! A staff member will be here shortly!\n `Ticket ID`:{role.mention}")
                 await ticket_channel.send(f"{user.mention} I have already pinged the `@Staff` team. No need for you to ping them.")
                 await reaction_message.remove_reaction(emoji, user)
 
         @commands.command()
         @commands.has_role("Staff")
         async def close(self, ctx):
-            """Closes a ticket"""
             await ctx.channel.delete()
 
         @commands.command()
         @commands.has_role('Staff')
         async def adduser(self, ctx, member: disnake.Member):
-            """Add a user to the ticket"""
             await ctx.message.delete()
             await ctx.channel.set_permissions(member, send_messages=True, read_messages=True, add_reactions=True, embed_links=True, attach_files=True, read_message_history=True, external_emojis=True)
             em = disnake.Embed(title="Add", description=f'{ctx.author.mention} has added {member.mention} to {ctx.channel.mention}')
@@ -57,11 +57,10 @@ class Tickets(commands.Cog):
         @commands.command()
         @commands.has_permissions(manage_messages=True)
         async def removeuser(self, ctx, member: disnake.Member):
-            """Remove a user from a ticket"""
             await ctx.message.delete()
             await ctx.channel.set_permissions(member, overwrite=None)
             em = disnake.Embed(title="Remove", description=f'{ctx.author.mention} has removed {member.mention} to {ctx.channel.mention}')
             await ctx.send(embed=em)
-
+    """
 def setup(client):
     client.add_cog(Tickets(client))
