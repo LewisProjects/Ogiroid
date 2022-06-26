3011990ea828bb0a6450a4d0191a2b1a4f92ae67
FreebieII <fredzapke9@gmail.com>
2022-06-26 20:23:34 +0200

    Base Tickets [Need to add HasTicketCheck]

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -1,125 +1,67 @@
-from disnake.ext import commands
 import disnake
+from disnake.ext import commands
 
 
 class Tickets(commands.Cog):
-    """🎫 Ticket Related Commands!"""
+        """🎫 Ticketing system commands (Staff)"""
+        def __init__(self, client):
+            self.client = client
 
-    def __init__(self, bot):
-        self.bot = bot
-
-    @property
-    def db(self):
-        return self.bot.db
-
-    """
-    async def check_if_user_id_is_in_db(self, user_id):
-        ""Checks if a user_id is in the database""
-        print(type(user_id))
-
-        async with self.db.execute("SELECT * FROM tickets") as cur:
-            async for user_id in cur:
-                print(user_id)
-                if user_id == cid:
-                    return True
-                else:
-                    return False
-                    print(data)
-        return bool(data)
-        async with self.db.execute("SELECT * FROM tags LIMIT 20") as cur:
-            async for tags in cur:
-                embed.add_field(
-                    name="Showing all available tags", value=f" `{tags[0]}`"
-                )
-        await ctx.send(embed=embed)
-"""
-
-    @commands.command()
-    @commands.guild_only()
-    async def ticket(self, ctx):  # , *, message):
-        """Create a ticket"""
-
-        creator = ctx.message.author
-        cid = creator.id
-        staff = disnake.utils.get(ctx.guild.roles, name="Staff")
-        # await ctx.send("You already have a ticket open!")
-
-        async with self.db.execute("SELECT * FROM tickets") as cur:
-            async for user_id in cur:
-                print(user_id[0])
-                if user_id == cid:
-                    await ctx.send("You already have a ticket open!")
-                    return True
-                else:
-
-                    # if await self.check_if_user_id_is_in_db(cid) == True:
-                    #    await ctx.send("You already have a ticket open!")
-                    # CHECK IF THERES A TICKET FOR THE  USER ALREADY
-                    channel = await ctx.guild.create_text_channel(
-                        name=f"ticket-{creator.name}", reason="Created a ticket"
-                    )
-                    await channel.set_permissions(
-                        ctx.guild.default_role, send_messages=False
-                    )
-                    await channel.set_permissions(ctx.guild.me, send_messages=True)
-                    await channel.set_permissions(creator, send_messages=True)
-                    await channel.set_permissions(
-                        staff, send_messages=True
-                    )  # Broken? I don't know why it seems to be dead.
-                    # Changing the channel Description:
-                    await channel.edit(
-                        topic=f"Created by: {creator.name}, ID: {creator.id}. Please use this channel to communicate with the staff team."
-                    )
-
-                    embed = disnake.Embed(
-                        title="Ticket Created!",
-                        description=f"{creator.mention}, a ticket has been created for you!",
-                        color=0xFFFFFF,
-                    )
-                    await ctx.send(embed=embed)
-                    await channel.send(
-                        f"{creator.mention}, you can access your ticket here!"
-                    )
-                    user_id = ctx.author.id
-                    await self.db.execute(
-                        "INSERT INTO tickets (user_id, channel_id) VALUES (?, ?)",
-                        [str(user_id), str(channel.id)],
-                    )
-                    await self.db.commit()
-                    user_id = ctx.author.id
-
-    """
-        embed = disnake.Embed(title="USER_IDS", description="USER_IDS", color=0x00FF00)
-        async with self.db.execute("SELECT user_id FROM tickets") as cur:
-            async for user_id in cur:
-                if user_id == ctx.author.id:
-                    await ctx.send(f"<@{user_id}> You already have a ticket open!")
-                    break
-                else:
-                    embed.add_field(name="USER_ID", value=f" `{user_id}`")
-                    await ctx.send(f"{user_id}")
-                    print(user_id)
-        await ctx.send(embed=embed)
-"""
-
-    @commands.command()
-    @commands.has_role("Staff")
-    async def close(self, ctx):
-        """Closes a ticket"""
-        await ctx.channel.delete()
-
-    """
-        embed = disnake.Embed(title="CHANNEL_IDS", description="CHANNEL_IDS", color=0xff0000)
-        #async with self.db.execute("SELECT * FROM tickets WHERE user_id = ?", [user_id]).fetchall()  as cur:
-        async with self.db.execute("SELECT channel_id FROM tickets LIMIT 20") as cur:
-            async for channel_id in cur:
-                embed.add_field(name='CHANNEL_IDS', value=f" `{channel_id}`")
-                await ctx.send(f"{channel_id}")
-                print(channel_id)
-        await ctx.send(embed=embed)
-      #  check = await self.check_if_user_id_is_in_db(user_id)
-"""
+        @commands.Cog.listener()
+        async def on_ready(self):
+            channel = self.client.get_channel(int(990679557596135475))
+            message = await channel.fetch_message(int(990679907795349554))
+            emoji = self.client.get_emoji(990310706874290216)
+            await message.add_reaction(emoji)
 
 
-def setup(bot):
-    bot.add_cog(Tickets(bot))
+
+
+
+        @commands.Cog.listener()
+        async def on_raw_reaction_add(self, payload):
+            reaction_message = await self.client.get_channel(990679557596135475).fetch_message(990679907795349554)
+            if payload.message_id == reaction_message.id:
+                user = self.client.get_user(payload.user_id)
+                if user.id == 984802008403959879:
+                    return print('Added reaction from bot')
+                    
+                staff = self.client.get_guild(985234686878023730).get_role(985943266115584010)
+                emoji = self.client.get_emoji(990310706874290216)
+                ticket_channel = await reaction_message.guild.create_text_channel(f"ticket-{user.name}")
+                await ticket_channel.set_permissions(reaction_message.guild.get_role(reaction_message.guild.id), read_messages=False)
+                await ticket_channel.set_permissions(user, send_messages=True, read_messages=True, add_reactions=True, embed_links=True, attach_files=True, read_message_history=True, external_emojis=True)
+                await ticket_channel.set_permissions(staff, send_messages=True, read_messages=True, add_reactions=True, embed_links=True, attach_files=True, read_message_history=True, external_emojis=True)
+                message_content = "Thank you for contacting support! A staff member will be here shortly!"
+                em = disnake.Embed(title=f"Ticket made by {user.name}#{user.discriminator}", description= f"{message_content}", color=0x26ff00)
+                em.set_footer(text="ticket", icon_url=user.avatar.url)
+                await ticket_channel.send(embed=em)
+                await ticket_channel.send(f"{user.mention} I have already pinged the `@Staff` team. No need for you to ping them.")
+                await reaction_message.remove_reaction(emoji, user)
+
+        @commands.command()
+        @commands.has_role("Staff")
+        async def close(self, ctx):
+            """Closes a ticket"""
+            await ctx.channel.delete()
+
+        @commands.command()
+        @commands.has_role('Staff')
+        async def adduser(self, ctx, member: disnake.Member):
+            """Add a user to the ticket"""
+            await ctx.message.delete()
+            await ctx.channel.set_permissions(member, send_messages=True, read_messages=True, add_reactions=True, embed_links=True, attach_files=True, read_message_history=True, external_emojis=True)
+            em = disnake.Embed(title="Add", description=f'{ctx.author.mention} has added {member.mention} to {ctx.channel.mention}')
+            await ctx.send(embed=em)
+
+        @commands.command()
+        @commands.has_permissions(manage_messages=True)
+        async def removeuser(self, ctx, member: disnake.Member):
+            """Remove a user from a ticket"""
+            await ctx.message.delete()
+            await ctx.channel.set_permissions(member, overwrite=None)
+            em = disnake.Embed(title="Remove", description=f'{ctx.author.mention} has removed {member.mention} to {ctx.channel.mention}')
+            await ctx.send(embed=em)
+
+def setup(client):
+    client.add_cog(Tickets(client))
