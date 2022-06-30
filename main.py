f0aa3dc5ab9fa6d4e9f70baa0e7e972d5bb30126
Freebie <69956546+FreebieII@users.noreply.github.com>
2022-06-30 10:45:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import aiosqlite
 import os
 from disnake.ext.commands import when_mentioned_or
 
-
-GUILD_ID = 985234686878023730  # 897666935708352582
-BUG_CHAN = 985554459948122142  # 982669110926250004
-SUGG_CHAN = 985554479405490216  # 982353129913851924
+GUILD_ID = 897666935708352582 
+BUG_CHAN = 982669110926250004  
+SUGG_CHAN = 982353129913851924 
 
 with open("setup.sql", "r") as sql_file:
     SETUP_SQL = sql_file.read()
@@ -22,15 +21,15 @@ class OGIROID(commands.Bot):
         await self.wait_until_ready()
         await self.change_presence(
             activity=disnake.Activity(
-                type=disnake.ActivityType.listening, name="the users!"
+                type=disnake.ActivityType.listening, name="v2.0 | Beta! | !!help"
             )
         )
         print(
             "--------------------------------------------------------------------------------"
         )
         print("Bot is ready! Logged in as: " + self.user.name)
-        print("Bot author: HarryDaDev | FreebieII")
-        print("Bot version: 1.3")
+        print("Bot author: HarryDaDev  |  FreebieII")
+        print("Bot version: 2.0 BETA")
         print(
             "--------------------------------------------------------------------------------"
         )
@@ -47,32 +46,16 @@ async def get_prefix(bot, message):
 
 
 client = OGIROID(
-    command_prefix=get_prefix,
-    intents=disnake.Intents.all(),
-    help_command=None,
-    sync_commands_debug=True,
+    command_prefix=get_prefix, intents=disnake.Intents.all(), help_command=None
 )
-
-"""
-@client.event
-async def on_command_error(ctx, error):
-	if isinstance(error, commands.CommandOnCooldown):
-		await ctx.send(f"**Slow down there!** \n{round(error.retry_after, 2)} seconds left.")
-"""
-
 
 
 def main():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             client.load_extension(f"cogs.{filename[:-3]}")
-    # client.run(
-    #    "ODYyMzU5NDIyNTY3NTc5NzA4.GUc6gD.gRe0vE8eL0UqzF_wTuQ80oAoFm7aZf1ilqfWY0"
-    # )  # MESSING AROUNDS TOKEN
     client.run(
-        "OTg0ODAyMDA4NDAzOTU5ODc5.GR1i_b.zc0G9MjPwXA8wcvf7rAx3OJpwvpOmZSKSnqh50"
-    )  # OGIROIDS TOKEN
-
-
+        "ODYyMzU5NDIyNTY3NTc5NzA4.GUc6gD.gRe0vE8eL0UqzF_wTuQ80oAoFm7aZf1ilqfWY0"
+    )  
 if __name__ == "__main__":
     main()
