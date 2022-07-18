9b050a986f93b7580c9dc79cbb1128fc5c494e9f
DWAA1660 <matnip12345@gmail.com>
2022-07-18 13:42:30 -0600

    made password generater send as euphermal if cannot dm user and removed inccorect code

--- cogs/under-development-cmds.py
+++ cogs/under-development-cmds.py
@@ -10,22 +10,6 @@ class development_cmds(commands.Cog):
     def __init__(self, bot: commands.AutoShardedBot):
         self.bot = bot
 
-    @commands.slash_command(
-        name="password", aliases=["pass"], description="Generate a random password & DM's it!"
-    )
-    async def password(inter, length: int):
-        """Generate a random password & DM's it!"""
-        if length > 100:
-            length = 100
-        password = "".join(secrets.choice(string.ascii_letters + string.digits) for _ in range(length))
-        #Checking if DM's are open, if they are, send the password to the user
-        if inter.author.dm_channel is None:
-            await inter.author.create_dm()
-            await inter.author.dm_channel.send(f"Your password is: `{password}`")
-            await inter.response.send_message("Your password has been sent!")
-        #If DM's are closed, send the password to the channel
-        else:
-            await inter.response.send(f"Your DM's are closed and due to safety reasons, your password cannot be sent to you. Please open your DM's and try again.")
 
 def setup(bot):
     bot.add_cog(development_cmds(bot))
