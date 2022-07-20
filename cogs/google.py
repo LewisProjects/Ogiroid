8219f03b7fb6ce8444a8f3e02f34bbaec2880275
LevaniVashadze <vashadzelevani11@gmail.com>
2022-07-20 19:02:43 +0400

    create google commands

---
+++ cogs/google.py
@@ -0,0 +1,23 @@
+from disnake.ext import commands
+import disnake
+
+
+class Google(commands.Cog):
+    """Google stuff"""
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.slash_command(description="Returns a google search for your query")
+    async def google(self, inter, query):
+        """Googles the keyword entered"""
+        await inter.send(f"https://www.google.com/search?q={query}")
+
+    @commands.slash_command(name="feeling-lucky", description="Returns the first google result for your query")
+    async def lucky(self, inter, query):
+        """Googles the keyword entered and returns the first result"""
+        await inter.send(f"https://www.google.com/search?q={query}&btnI")
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Google(bot))
