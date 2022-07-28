f49d5d3fa2fe5ce6d5a3122037d87febd6f03068
Jason <thebosssg19@gmail.com>
2022-07-27 22:06:46 -0400

    added a TagsNotFound exception

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -6,5 +6,9 @@ class TagNotFound(TagException, KeyError):
     pass
 
 
+class TagsNotFound(TagException):
+    pass  # used when no tags are found i.e when the user has no tags or when there are no tags in the database
+
+
 class TagAlreadyExists(TagException):
     pass
