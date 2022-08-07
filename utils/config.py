126364a53aecdfe455340a2a996dcac9255ca842
Freebie <69956546+FreebieII@users.noreply.github.com>
2022-08-07 21:14:52 +0200

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,7 @@ from utils.CONSTANTS import *
 
 @dataclass
 class Config:
-    Development = True  # if true will use base server ID's else will use development server ID's
+    Development = False  # if true will use base server ID's else will use development server ID's
     if Development:
         print("Using Development Config variable")
         channels = Channels.dev()
