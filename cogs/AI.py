f0c5d89daf33cccf8c4c00aa4108cc425183d26e
LevaniVashadze <vashadzelevani11@gmail.com>
2022-10-22 20:06:49 +0400

    made ephemeral

--- cogs/AI.py
+++ cogs/AI.py
@@ -8,20 +8,21 @@ from disnake.ext import commands
 from utils.bot import OGIROID
 
 
-class AI(commands.Cog):
-    def __init__(self, bot: OGIROID):
-        self.bot = bot
-
-    # @commands.slash_command(description="Generates ai art")
-    # async def ai_art(self, inter: disnake.ApplicationCommandInteraction, text):
-    #     ETA = int(time.time() + 60)
-    #     await inter.send(f"Go grab a coffee this may take a while... ETA: <t:{ETA}:R>", delete_after=69)
-    #     generator = Craiyon()
-    #     result = generator.generate(text)
-    #     images = result.images
-    #     for i in images:
-    #         image = BytesIO(base64.decodebytes(i.encode("utf-8")))
-    #         return await inter.channel.send("Have fun...", file=disnake.File(image, "image.png"))
+# class AI(commands.Cog):
+#     def __init__(self, bot: OGIROID):
+#         self.bot = bot
+#
+#     @commands.slash_command(description="Generates ai art")
+#     async def ai_art(self, inter: disnake.ApplicationCommandInteraction, text):
+#         ETA = int(time.time() + 60)
+#         await inter.send(f"Go grab a coffee this may take a while... ETA: <t:{ETA}:R>")
+#         generator = Craiyon()
+#         result = generator.generate(text)
+#         images = result.images
+#         for i in images:
+#             image = BytesIO(base64.decodebytes(i.encode("utf-8")))
+#             await inter.delete_original_response()
+#             return await inter.send("Have fun...", file=disnake.File(image, "image.png"), ephemeral=True)
 
 
 def setup(bot):
