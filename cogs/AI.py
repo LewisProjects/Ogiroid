3b42b687e997f2ac333e8263cec209e5b8ca1fa5
LevaniVashadze <vashadzelevani11@gmail.com>
2022-10-22 21:45:05 +0400

    fuck craiyon.py do it yourself and in async

--- cogs/AI.py
+++ cogs/AI.py
@@ -1,4 +1,3 @@
-from craiyon import Craiyon
 import base64
 from PIL import Image
 from io import BytesIO
@@ -12,17 +11,17 @@ class AI(commands.Cog):
     def __init__(self, bot: OGIROID):
         self.bot = bot
 
-#     @commands.slash_command(description="Generates ai art")
-#     async def ai_art(self, inter: disnake.ApplicationCommandInteraction, text):
-#         ETA = int(time.time() + 60)
-#         await inter.send(f"Go grab a coffee this may take a while... ETA: <t:{ETA}:R>")
-#         generator = Craiyon()
-#         result = generator.generate(text)
-#         images = result.images
-#         for i in images:
-#             image = BytesIO(base64.decodebytes(i.encode("utf-8")))
-#             await inter.delete_original_response()
-#             return await inter.send("Have fun...", file=disnake.File(image, "image.png"), ephemeral=True)
+    @commands.slash_command(description="Generates ai art")
+    async def ai_art(self, inter: disnake.ApplicationCommandInteraction, text):
+        ETA = int(time.time() + 60)
+        await inter.send(f"Go grab a coffee this may take a while... ETA: <t:{ETA}:R>")
+        response = await self.bot.session.post("https://backend.craiyon.com/generate", json={'prompt': text})
+        r = await response.json()
+        images = r['images']
+        for i in images:
+            image = BytesIO(base64.decodebytes(i.encode("utf-8")))
+            await inter.delete_original_response()
+            return await inter.send("Have fun...", file=disnake.File(image, "image.png"), ephemeral=True)
 
 
 def setup(bot):
