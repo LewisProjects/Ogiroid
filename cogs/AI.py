8780b0504783988f3d6ff3f6213bf0f005ddcd62
LevaniVashadze <vashadzelevani11@gmail.com>
2022-11-12 23:04:22 +0400

    refactor

--- cogs/AI.py
+++ cogs/AI.py
@@ -16,13 +16,10 @@ class AI(commands.Cog):
         await inter.send(f"Go grab a coffee this may take a while... ETA: <t:{ETA}:R>", ephemeral=True)
         response = await self.bot.session.post("https://backend.craiyon.com/generate", json={"prompt": text})
         r = await response.json()
-        images = r["images"]
-        for i in images:
-            image = BytesIO(base64.decodebytes(i.encode("utf-8")))
-            await inter.send("Have fun", ephemeral=True)
-            await inter.send(file=disnake.File(image, "image.png"), ephemeral=True)
+        raw_images = r["images"]
+        images = [disnake.File(BytesIO(base64.decodebytes(i.encode("utf-8"))), "image.png") for i in raw_images]
 
-        return await inter.delete_original_response()
+        return await inter.edit_original_response(files=images, content="Here you go!")
 
 
 def setup(bot):
