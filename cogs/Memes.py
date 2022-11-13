db7b29a812eb33b89e26b53bbfa7881d1576d3bf
nxtCP <117382123+nxtCP@users.noreply.github.com>
2022-11-13 12:44:55 -0800

    Update Memes.py

--- cogs/Memes.py
+++ cogs/Memes.py
@@ -19,7 +19,7 @@ class Memes(commands.Cog):
             "https://media.wired.com/photos/59548ac98e8cc150fa8ec379/master/w_2560%2Cc_limit/GettyImages-56196238.jpg"
         )
     @commands.slash_command(name="boners with lewis", description="Boners with lewis")
-    async def onlyfans(self, inter):
+    async def bwl(self, inter):
         """Boners with lewis"""
         await inter.send(
             "https://pbs.twimg.com/media/FhDemilXEAI_CNx.jpg"
