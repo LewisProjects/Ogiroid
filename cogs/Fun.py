c3b9c5b6951c9f826a190e92043631a5ce17a0b4
CordlessCoder <>
2022-11-20 19:49:24 +0000

    possibly working url shortener idk

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -405,10 +405,10 @@ class Fun(commands.Cog):
     @commands.slash_command(name="urlshortner", description="Shortens a URL.")
     async def urlshortner(self, inter: ApplicationCommandInteraction, url: str):
         # checking if url starts with http:// or https://, if it does not, adding https:// towards the start
-        if not url.startswith("http://") and not url.startswith("https://"):
+        if not (url.startswith("http://") and url.startswith("https://")):
             url = f"https://{url}"
-        response = requests.post(f"https://u.jasoncodes.ca/add/{url}")
-        if response.status_code == 200:
+        response = requests.post("https://roman.vm.net.ua/s", url)
+        if response.status_code == 201:
             embed = disnake.Embed(
                 title=f"URL created for: {url.replace('http://', '').replace('https://', '')}",
                 color=0xFFFFFF,
