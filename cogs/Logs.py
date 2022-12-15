ca2d66312a93b81eeb88fb715790a215d9dbc729
Giorgi Merebashvili <abracadabragio12@gmail.com>
2022-12-15 21:07:51 +0400

    added new log events

--- cogs/Logs.py
+++ cogs/Logs.py
@@ -3,11 +3,45 @@ from datetime import datetime
 from disnake import Embed
 from disnake.ext.commands import Cog
 
+import discord
+import typing as t
+
+MAIN_COLOR = 0x5e7bdd
+POSITIVE_COLOR = 0x309c41
+NEGATIVE_COLOR = 0Xcc0202
+
 
 class Log(Cog):
     def __init__(self, bot):
         self.bot = bot
 
+   async def send_modlog(self,
+        color: discord.Colour,
+        title: t.Optional[str],
+        text: str,
+        member: t.Optional[discord.Member] = None,
+        content: t.Optional[str] = None) -> Context:
+    
+        embed = discord.Embed(description=text[:4093] + '...' if len(text) > 4096 else text)
+
+        if title:
+            embed.set_author(name=title)
+
+        embed.timestamp = datetime.utcnow()
+        embed.color = color
+        
+        
+        if content and len(content) > 2000:
+            content = content[:2000 - 3] + '...'
+        
+        modlog_channel = self.bot.get_channel(self.bot.config.channels.logs)
+        log_message = await modlog_channel.send(
+            content=content,
+            embed=embed
+        )
+
+        return log_message     
+        
     @Cog.listener()
     async def on_user_update(self, before, after):
         log_channel = self.bot.get_channel(self.bot.config.channels.logs)
@@ -180,6 +214,81 @@ class Log(Cog):
             text=f"{inter.author.name}#{inter.author.discriminator}"
         )
         await log_channel.send(embed=embed)
+
+    @Cog.listener()
+    async def on_guild_role_delete(self, role: discord.Role):
+        """Sends a message in log channel when role is deleted."""
+        title = 'Role deleted'
+        content = f'**{role.name}**(``{role.id}``) has been deleted.'
+
+        await self.send_modlog(NEGATIVE_COLOR, title, content)
+
+
+    @Cog.listener()
+    async def on_guild_role_update(self, before: discord.Role, after: discord.Role):
+
+        title = 'Role edited'
+        after.permissions = []
+        before.permissions = []
+        
+        if before.permissions == after.permissions:
+            return
+
+        if before.name != after.name: 
+            content = f'**{before.name}** has been named to **{after.name}**'
+        else: 
+            return
+        
+        await self.send_modlog(MAIN_COLOR, title, content)
+
+    
+    @Cog.listener()
+    async def on_guild_update(self, before: discord.Guild, after: discord.Guild):
+
+        title = 'Server edited'
+        
+        if before.name != after.name:
+            message = (f'Server name has been changed:\n'
+                       f'Before: `{before.name}`\n'
+                       f'After: `{after.name}`')
+        elif before.afk_channel != after.afk_channel:
+            message = (f'Server AFK channel has been edited:\n'
+                       f'Before: {before.afk_channel.mention}\n'
+                       f'After: {after.afk_channel.mention}')
+        elif before.afk_timeout != after.afk_timeout:
+            message = (f'Server AFK timeout has been edited:\n'
+                       f'Before: `{before.afk_timeout}`\n'
+                       f'After: `{after.afk_timeout}`')
+
+        await self.send_modlog(MAIN_COLOR, title, message)
+
+    @Cog.listener()
+    async def on_thread_create(self, thread: discord.Thread):
+
+        title = 'Thread created'
+        message = f'Thread {thread.mention} (`{thread.id}`) has been created.'
+
+        await self.send_modlog(POSITIVE_COLOR, title, message)
+
+
+    @Cog.listener()
+    async def on_thread_update(self, before: discord.Thread, after: discord.Thread):
+
+        title = 'Thread name edited'
+        message = (f'Thread {after.mention} (`{after.id}`) in {after.parent.mention} (`{after.parent.id}`)\n'
+                   f'Before: `{before.name}`\n' 
+                   f'After: `{after.name}`')
+
+        await self.send_modlog(MAIN_COLOR, title, message)
+
+    
+    @Cog.listener()
+    async def on_thread_delete(self, thread: discord.Thread):
+
+        title = 'Thread deleted'
+        message = f'Thread **{thread.name}** (`{thread.id}`) in {thread.parent.mention} (`{thread.parent.id}`) deleted.'
+
+        await self.send_modlog(NEGATIVE_COLOR, title, message)
 
 
 def setup(bot):
