f1d573482c6e555b33a397b466e2b76281c70564
LevaniVashadze <vashadzelevani11@gmail.com>
2023-09-17 15:13:05 +0400

    ai command disable

--- cogs/AI.py
+++ cogs/AI.py
@@ -13,36 +13,33 @@ class AI(commands.Cog):
         self.bot = bot
 
     @commands.slash_command(description="Generates ai art")
-    async def ai_art(
-        self, inter: disnake.ApplicationCommandInteraction, text: str
-    ):
-        if profanity.contains_profanity(text):
-            return await inter.send(
-                f"NSFW requests are not allowed!", ephemeral=True
-            )
-        if "bot" in inter.channel.name or "command" in inter.channel.name:
-            hidden = False
-        else:
-            hidden = True
-        ETA = int(time.time() + 60)
+    async def ai_art(self, inter: disnake.ApplicationCommandInteraction, text: str):
+        # if profanity.contains_profanity(text):
+        #     return await inter.send(f"NSFW requests are not allowed!", ephemeral=True)
+        # if "bot" in inter.channel.name or "command" in inter.channel.name:
+        #     hidden = False
+        # else:
+        #     hidden = True
+        # ETA = int(time.time() + 60)
+        # await inter.send(
+        #     f"Go grab a coffee this may take a while... ETA: <t:{ETA}:R>",
+        #     ephemeral=hidden,
+        # )
+        # response = await self.bot.session.post(
+        #     "https://backend.craiyon.com/generate", json={"prompt": text}
+        # )
+        # print(response.text)
+        # r = await response.json()
+        # raw_images = r["images"]
+        # images = [
+        #     disnake.File(BytesIO(base64.decodebytes(i.encode("utf-8"))), "image.png")
+        #     for i in raw_images
+        # ]
+        #
+        # await inter.edit_original_response(files=images, content="Here you go!")
         await inter.send(
-            f"Go grab a coffee this may take a while... ETA: <t:{ETA}:R>",
-            ephemeral=hidden,
-        )
-        response = await self.bot.session.post(
-            "https://backend.craiyon.com/generate", json={"prompt": text}
-        )
-        r = await response.json()
-        raw_images = r["images"]
-        images = [
-            disnake.File(
-                BytesIO(base64.decodebytes(i.encode("utf-8"))), "image.png"
-            )
-            for i in raw_images
-        ]
-
-        await inter.edit_original_response(
-            files=images, content="Here you go!"
+            "This command is currently disabled due to the Craiyon API not working anymore",
+            ephemeral=True,
         )
 
 
