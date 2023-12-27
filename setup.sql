7285a0dee25cc39d1dc0b054bb40879c15d97a08
LevaniVashadze <vashadzelevani11@gmail.com>
2023-12-27 17:46:45 +0400

    add ID to each table

--- setup.sql
+++ setup.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tags
 (
-    tag_id TEXT,
+    tag_id TEXT PRIMARY KEY,
     content TEXT,
     owner BIGINT,
     created_at BIGINT,
@@ -9,12 +9,13 @@ CREATE TABLE IF NOT EXISTS tags
 
 CREATE TABLE IF NOT EXISTS tag_relations
 (
-    tag_id TEXT,
+    tag_id TEXT PRIMARY KEY,
     alias TEXT
 );
 
 CREATE TABLE IF NOT EXISTS blacklist
 (
+    id SERIAL PRIMARY KEY,
     user_id BIGINT,
     reason TEXT,
     bot BOOLEAN,
@@ -25,6 +26,7 @@ CREATE TABLE IF NOT EXISTS blacklist
 
 CREATE TABLE IF NOT EXISTS flag_quiz
 (
+    id BIGINT PRIMARY KEY,
     user_id BIGINT,
     tries INTEGER,
     correct INTEGER,
@@ -34,7 +36,7 @@ CREATE TABLE IF NOT EXISTS flag_quiz
 
 CREATE TABLE IF NOT EXISTS trivia
 (
-    id BIGINT,
+    id BIGINT PRIMARY KEY,
     correct INTEGER,
     incorrect INTEGER,
     streak INTEGER,
@@ -43,6 +45,7 @@ CREATE TABLE IF NOT EXISTS trivia
 
 CREATE TABLE IF NOT EXISTS reaction_roles
 (
+    id SERIAL PRIMARY KEY,
     message_id BIGINT,
     role_id BIGINT,
     emoji TEXT,
@@ -60,6 +63,7 @@ CREATE TABLE IF NOT EXISTS warnings
 
 CREATE TABLE IF NOT EXISTS levels
 (
+    id SERIAL PRIMARY KEY,
     guild_id BIGINT,
     user_id BIGINT,
     level INTEGER DEFAULT 0,
@@ -68,6 +72,7 @@ CREATE TABLE IF NOT EXISTS levels
 
 CREATE TABLE IF NOT EXISTS role_rewards
 (
+    id SERIAL PRIMARY KEY,
     guild_id BIGINT,
     role_id BIGINT,
     required_lvl INTEGER DEFAULT 0
@@ -75,6 +80,7 @@ CREATE TABLE IF NOT EXISTS role_rewards
 
 CREATE TABLE IF NOT EXISTS birthday
 (
+    id SERIAL PRIMARY KEY,
     user_id BIGINT,
     birthday TEXT DEFAULT NULL,
     birthday_last_changed BIGINT DEFAULT NULL
@@ -82,6 +88,7 @@ CREATE TABLE IF NOT EXISTS birthday
 
 CREATE TABLE IF NOT EXISTS timezone
 (
+    id SERIAL PRIMARY KEY,
     user_id BIGINT,
     timezone TEXT DEFAULT NULL,
     timezone_last_changed BIGINT DEFAULT NULL
@@ -89,7 +96,7 @@ CREATE TABLE IF NOT EXISTS timezone
 
 CREATE TABLE IF NOT EXISTS config
 (
-    guild_id BIGINT,
+    guild_id BIGINT PRIMARY KEY,
     xp_boost INTEGER DEFAULT 1,
     xp_boost_expiry BIGINT DEFAULT 0,
     xp_boost_enabled BOOLEAN DEFAULT TRUE,
@@ -98,6 +105,7 @@ CREATE TABLE IF NOT EXISTS config
 
 CREATE TABLE IF NOT EXISTS commands
 (
+    id SERIAL PRIMARY KEY,
     guild_id BIGINT,
     command TEXT,
     command_used INTEGER DEFAULT 0,
@@ -106,6 +114,7 @@ CREATE TABLE IF NOT EXISTS commands
 
 CREATE TABLE IF NOT EXISTS total_commands
 (
+    id SERIAL PRIMARY KEY,
     guild_id  BIGINT UNIQUE,
     total_commands_used INTEGER DEFAULT 0
 );
